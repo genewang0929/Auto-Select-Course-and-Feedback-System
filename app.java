d3f1bee7f29d08f94415df5a75f1eb800caab96c
28921085 <83084203+28921085@users.noreply.github.com>
2021-05-05 15:48:50 +0800

    Add files via upload
    main app

---
+++ app.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class app {
+    public static void main(String args[]){
+        //主介面
+        JFrame mainApp=new JFrame("自動選課小幫手");
+        mainApp.setSize(500,500);
+        mainApp.setVisible(true);
+        mainApp.setLocationRelativeTo(null);
+
+    }
+}
