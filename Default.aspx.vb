293cad666d3bd084ee1aa435b6565a349f2421b1
YiChiaPeng <79035877+YiChiaPeng@users.noreply.github.com>
2021-05-12 17:02:40 +0800

    Add files via upload

---
+++ Default.aspx.vb
@@ -0,0 +1,8 @@
+﻿
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub idFindClassButton_Click(sender As Object, e As EventArgs) Handles idFindClassButton.Click
+
+    End Sub
+End Class
