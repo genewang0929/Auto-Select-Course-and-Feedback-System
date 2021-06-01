b5ff3bb4eadae719b3142d50248d205670716e6f
genewang0929 <genewang7@gmail.com>
2021-06-01 19:37:29 +0800

    Update selenium.java

--- src/selenium.java
+++ src/selenium.java
@@ -12,6 +12,7 @@ public class selenium {
     public static  void setDriver(){
         System.setProperty("webdriver.chrome.driver", "./resources/chromedriver.exe");
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
     }
     public static void loginNTOU(String Account, String Password) {
         setDriver();
@@ -22,33 +23,29 @@ public class selenium {
         password.sendKeys(Password);
         driver.findElement(By.xpath("//*[@id=\"LGOIN_BTN\"]")).click();
 
-        //上教學務爬取登入者姓名
-        //String name = String.valueOf(driver.findElement(By.xpath("//*[@id=\"USERNAME\"]")));
-        //System.out.println(name);
-        //TODO
-
         //將登入資料傳送到資料庫
 //        connectToSQL connectToSQL = new connectToSQL();
 //        connectToSQL.studentInfo(Account, Password, Name);
     }
+
     public static void acessToSelectClass(){
         //driver.get("https://ais.ntou.edu.tw/Application/TKE/TKE22/TKE2211_01.aspx");
         //還是用正規方法比較好
         driver.switchTo().frame("menuFrame");
         driver.findElement(By.xpath("//*[@id=\"Menu_TreeViewt1\"]")).click();
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        sleep(1);
         driver.findElement(By.xpath("/html/body/form/div[3]/table/tbody/tr[1]/td/div/div/div/table[3]/tbody/tr/td[5]/a")).click();
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        sleep(1);
         driver.findElement(By.xpath("/html/body/form/div[3]/table/tbody/tr[1]/td/div/div/div/div/table[2]/tbody/tr/td[6]/a")).click();
         //bs4去爬資料
         //結合批次選課
+    }
+
+    public static void sleep(int sec) {
+        try {
+            Thread.sleep(sec * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
