6cd937f7052c027543798fa44f2e99b75ae2a3e4
genewang0929 <genewang7@gmail.com>
2021-06-01 18:54:50 +0800

    Update insertInfo.java

--- src/insertInfo.java
+++ src/insertInfo.java
@@ -1,7 +1,7 @@
 public class insertInfo {
     private String query;
     public insertInfo() {
-        query = "INSERT INTO StudentInfo(user_id, password_, studentName) VALUES(?, ?, ?)";
+        query = "INSERT INTO StudentInfo(user_id, password_, ban) VALUES(?, ?, ?)";
     }
 
     public String getQuery() {
