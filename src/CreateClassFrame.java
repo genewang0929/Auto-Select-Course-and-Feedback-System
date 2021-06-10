f7c0979fc94e4b67dc6bfd8fe1af764330b59e61
genewang0929 <genewang7@gmail.com>
2021-06-10 23:22:27 +0800

    Update CreateClassFrame.java

--- src/CreateClassFrame.java
+++ src/CreateClassFrame.java
@@ -12,6 +12,7 @@ public class CreateClassFrame extends JFrame implements ActionListener {
     private JTextField inputClass;
     private JTextArea inputComment;
     private JButton submit;
+    private JButton back;
     private String userName;
     public CreateClassFrame(String userName) {
         super("評論區(目前登入:" + userName + ")");
@@ -41,7 +42,12 @@ public class CreateClassFrame extends JFrame implements ActionListener {
 
         submit = new JButton("確認");
         submit.addActionListener(this);
-        add(submit, BorderLayout.SOUTH);
+        back = new JButton("回上頁");
+        back.addActionListener(this);
+        JPanel panel = new JPanel(new GridLayout(1, 2));
+        panel.add(back);
+        panel.add(submit);
+        add(panel, BorderLayout.SOUTH);
         setVisible(true);
     }
 
@@ -51,6 +57,11 @@ public class CreateClassFrame extends JFrame implements ActionListener {
             ConnectToSQL conn = new ConnectToSQL();
             conn.createClass(userName, inputComment.getText(), inputClass.getText());
             dispose();
+        }
+        else {
+            dispose();
+            commentAreaFrame caf = new commentAreaFrame(userName);
+            caf.open();
         }
     }
 }
