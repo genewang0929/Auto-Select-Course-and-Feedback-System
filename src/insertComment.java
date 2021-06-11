059196f94fc5320f91b6f6129d25c7d86bbb1a65
genewang0929 <genewang7@gmail.com>
2021-06-11 12:27:34 +0800

    Update insertComment.java

--- src/insertComment.java
+++ src/insertComment.java
@@ -1,7 +1,7 @@
 public class InsertComment {
     private String query;
     public InsertComment(String className) {
-        query = "INSERT INTO " + className + "(username, comment_, created_on)"
+        query = "INSERT INTO " + className + "(user_id, comment_, created_on)"
                 + "VALUES (?, ?, CURRENT_TIMESTAMP)";
     }
 
