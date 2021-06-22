0c25aa32f4fd732cca55866376197c2a1d0bd911
YiChiaPeng <79035877+YiChiaPeng@users.noreply.github.com>
2021-06-22 20:50:28 +0800

    Add files via upload

--- src/warning.java
+++ src/warning.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.Timer;
@@ -14,8 +16,18 @@ public class warning extends JFrame {
     public void open(){
         warning=new JFrame();
         warning.setTitle("提示");
-        warning.setSize(200,100);
-        JLabel label=new JLabel("課程"+className+"已經可以選了");
+        warning.setSize(300,200);
+        warning.setLayout(null);
+        JLabel label=new JLabel("課程"+className+"已經可以選了，是否加選?");
+        label.setBounds(0,50,300,30);
+        JButton yes=new JButton("是");
+        yes.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                warning.dispose();
+            }
+        });
+
         warning.add(label);
         warning.setLocationRelativeTo(null);
         warning.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
@@ -25,8 +37,20 @@ public class warning extends JFrame {
                 Toolkit.getDefaultToolkit().beep();
             }
         };
+        JButton no=new JButton("忍痛放棄");
+        no.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                warning.dispose();
+                task.cancel();
+            }
+        });
+        yes.setBounds(60,80,50,30);
+        no.setBounds(120,80,100,30);
+        warning.add(yes);
+        warning.add(no);
         Timer timer=new Timer();
-        timer.schedule(task,0,200);
+        timer.schedule(task,0,400);
         warning.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -37,8 +61,4 @@ public class warning extends JFrame {
         warning.setVisible(true);
 
     }
-    /*public static void main(String[] args){
-        warning a=new warning("軟體工程");
-        a.open();
-    }*/
 }
